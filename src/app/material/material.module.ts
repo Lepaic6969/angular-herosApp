b8b91c6a0c113153413459e1a6e9d41ad5bf8d54
Juan Camilo Paredes Zamora <jparedesz@dian.gov.co>
2024-11-04 21:52:39 -0500

    Enrutamiento del proyecto listo

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+//En este módulo sólo se importan y exportan los módulos del Angular Material
+import { NgModule } from '@angular/core';
+
+@NgModule({
+ exports:[
+
+ ]
+})
+export class MaterialModule { }
